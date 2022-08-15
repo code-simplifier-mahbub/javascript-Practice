b226d59b55d4ad983a22efea71f3d1f7f9c17cad
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-08-15 18:39:06 +0600

    intregrate javascript start

---
+++ javascript-intregat/dom/selector.js
@@ -0,0 +1,36 @@
+const show = document.querySelectorAll("h4");
+//  console.log(show);
+
+ const parti = document.getElementById("particular");
+//  console.log(parti);
+
+
+const different = document.getElementsByClassName("select");
+ for(const diff of differet){
+   //  console.log(diff)
+ }
+
+//  li.innerHTML
+
+// li.innerText
+// 'la casa da\nla casa da\nla casa da\nla casa da\nla casa da'
+
+// li.classList
+// DOMTokenList(5) ['select', 'korte', 'hobe', 'sobai', 'ke', value: 'select korte hobe sobai ke']
+
+// li.getAttribute("class")
+// 'select korte hobe sobai ke'
+
+// li.getAttribute("class")[1]
+// 'e'
+
+// li.setAttribute
+// ƒ setAttribute() { [native code] }
+
+// li.setAttribute("title", "abar kar pala")
+// undefined
+
+// li.style.color ="red"
+// 'red'
+
+// li.style.backgroundColor = "yellow"
