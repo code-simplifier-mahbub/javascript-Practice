1588c828918a49bf572dea35251aa8a937ee9f96
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-08-15 19:05:48 +0600

    javascript styles added

---
+++ javascript-intregat/dom/addStyle.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll("section");
+for(const section of sections){
+    section.style.backgroundColor = 'steelblue';
+    section.style.color = 'white';
+    section.style.borderRadius = '15px';
+    section.style.border = '3px solid black';
+    section.style.marginBottom = '9px';
+    section.style.padding = '30px'
+}
+
+const rongs = document.getElementById("styleAdd");
+// for(const rong of rongs){
+//     rong.style.backgroundColor = 'red';
+// }
+
+rongs.style.backgroundColor = 'lightblue';
+rongs.style.textAlign = 'center';
