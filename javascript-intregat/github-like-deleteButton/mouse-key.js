e802c783f66c536042d793d272b45abfb8625a88
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-08-18 01:33:40 +0600

    comment like github

---
+++ javascript-intregat/github-like-deleteButton/mouse-key.js
@@ -0,0 +1,43 @@
+// mouse interect
+
+// document.getElementById('interactive').addEventListener('mouseover', function(){
+//     console.log('hi wi are work for you')
+// })
+
+// document.getElementById('interactive').addEventListener('mousemove', function(){
+//     console.log('hi wi are work for you')
+// })
+document.getElementById('interactive').addEventListener('mouseout', function(){
+    console.log('hi wi are work for you')
+})
+
+//key interect
+document.getElementById('textField').addEventListener('keydown', function(event){
+    console.log(event.target.value)
+})
+// document.getElementById('textField').addEventListener('keyup', function(event){
+//     console.log(event.target.value)
+// })
+// document.getElementById('textField').addEventListener('keypress', function(event){
+//     console.log(event.target.value)
+// })
+
+// document.getElementById('textField').addEventListener('keypress', function(event){
+//         console.log(event)
+//     })
+
+// document.getElementById('textField').addEventListener('keypress', function(event){
+//     console.log(event.target.value)
+// })
+
+// document.getElementById('textField').addEventListener('focus', function(){
+//     console.log('hi guddu')
+// })
+// document.getElementById('textField').addEventListener('blur', function(){
+//     console.log('hi guddu blur')
+// })
+
+
+
+
+
